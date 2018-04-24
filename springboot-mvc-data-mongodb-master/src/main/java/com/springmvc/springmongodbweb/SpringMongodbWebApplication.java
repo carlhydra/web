c924526a6a4d3d3dd91b4970ccf1ae7fec4b3131
carlgeorge <carlgeorge@carls-mbp-6>
2018-04-24 17:06:59 -0400

    pom

--- springboot-mvc-data-mongodb-master/src/main/java/com/springmvc/springmongodbweb/SpringMongodbWebApplication.java
+++ springboot-mvc-data-mongodb-master/src/main/java/com/springmvc/springmongodbweb/SpringMongodbWebApplication.java
@@ -1,16 +1,18 @@
 package com.springmvc.springmongodbweb;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+//import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.SecurityAutoConfiguration;
+//import org.springframework.boot.autoconfigure.security.SecurityAutoConfiguration;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
+//import org.springframework.boot.context.web.SpringBootServletInitializer;
+
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @SpringBootApplication
 @EnableMongoRepositories("com.springmvc.springmongodbweb.repositories")
-@EnableAutoConfiguration(exclude = {SecurityAutoConfiguration.class})
+//@EnableAutoConfiguration(exclude = {SecurityAutoConfiguration.class})
 public class SpringMongodbWebApplication extends SpringBootServletInitializer {
 
     @Override
