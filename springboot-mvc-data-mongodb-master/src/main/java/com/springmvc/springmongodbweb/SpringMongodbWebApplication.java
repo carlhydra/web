4c93d655f3b98aff32d79e3340ad9b482662e1eb
carlgeorge <carlgeorge@carls-mbp-6>
2018-04-24 00:47:06 -0400

    test

--- springboot-mvc-data-mongodb-master/src/main/java/com/springmvc/springmongodbweb/SpringMongodbWebApplication.java
+++ springboot-mvc-data-mongodb-master/src/main/java/com/springmvc/springmongodbweb/SpringMongodbWebApplication.java
@@ -4,15 +4,30 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.SecurityAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @SpringBootApplication
 @EnableMongoRepositories("com.springmvc.springmongodbweb.repositories")
 @EnableAutoConfiguration(exclude = {SecurityAutoConfiguration.class})
+public class SpringMongodbWebApplication extends SpringBootServletInitializer {
 
-public class SpringMongodbWebApplication {
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(SpringMongodbWebApplication.class);
+    }
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(SpringMongodbWebApplication.class, args);
+    }
+
+}
+/*public class SpringMongodbWebApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringMongodbWebApplication.class, args);
 	}
-}
+}*/
+
+
